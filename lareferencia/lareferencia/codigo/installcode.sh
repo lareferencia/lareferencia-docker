48bfbd1b4988296abcf9e5667f98b9c767419d57
Tiago Alves de Oliveira <tiagofga@gmail.com>
2022-07-05 12:03:51 -0300

    fix: :hammer: Updating configs to run in correct directory

--- lareferencia/lareferencia/codigo/installcode.sh
+++ lareferencia/lareferencia/codigo/installcode.sh
@@ -3,5 +3,5 @@
 mkdir lrharvester
 cd lrharvester || exit
 git clone https://github.com/lareferencia/lareferencia-platform.git
-cd lareferencia-platform-lite || exit
+cd lareferencia-platform || exit
 bash pull_and_build.sh ibict
