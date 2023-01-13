4548e90215341cc1083e21f54a5064c250ba1b33
Tiago Alves de Oliveira <tiagofga@gmail.com>
2023-01-13 09:27:09 -0300

    fix: :memo: Updating installation to use oauth with user and password

--- lareferencia/lareferencia/codigo/installcode.sh
+++ lareferencia/lareferencia/codigo/installcode.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
-model=/home/lareferencia/lrharvester/lareferencia-entity-shell/config/application.properties.model
-properties=/home/lareferencia/lrharvester/lareferencia-entity-shell/config/application.properties
-
-# model=lareferencia-shell/config/application.properties.model
-# properties=lareferencia-shell/config/application.properties
-# if [ $# -ne 2 ]; then
+# model=/home/lareferencia/lrharvester/lareferencia-entity-shell/config/application.properties.model
+# properties=/home/lareferencia/lrharvester/lareferencia-entity-shell/config/application.properties
+# if [ $# -ne 0 ]; then
 #   echo "Number of parameters wrong"
-#   echo "Usage: $0 <user> <password>"
+#   echo "Usage: $0"
 #   exit 1
-# else
-#   user=$1
-#   password=$2
 # fi
-
-if [ $# -ne 0 ]; then
+model=lareferencia-shell/config/application.properties.model
+properties=lareferencia-shell/config/application.properties
+if [ $# -ne 2 ]; then
   echo "Number of parameters wrong"
-  echo "Usage: $0"
+  echo "Usage: $0 <user> <password>"
   exit 1
+else
+  user=$1
+  echo $user
+  password=$2
+  echo $password
 fi
-echo "Removing lrharverster folder"
-rm -rf lrharvester
-echo "Downloading lrharvester source"
-wget https://www.dropbox.com/s/29745jdtnsfa2g5/lrharvester.zip?dl=0 -O lrharvester.zip
-echo "Unzip files"
-unzip lrharvester.zip
-echo "Building lrharvester"
-cd lrharvester || exit
-./build.sh
-echo "Finished building lrharvester"
 
 # echo "Removing lrharverster folder"
-# rm -rf lrharvester/*
-# echo "Creating lrharvester folder"
-# mkdir -p lrharvester
-# echo "Changing diretory to lrharvester"
+# rm -rf lrharvester
+# echo "Downloading lrharvester source"
+# wget https://www.dropbox.com/s/29745jdtnsfa2g5/lrharvester.zip?dl=0 -O lrharvester.zip
+# echo "Unzip files"
+# unzip lrharvester.zip
+# echo "Building lrharvester"
 # cd lrharvester || exit
-# echo "Downloading lrharvester"
-# git clone https://$user:$password@github.com/lareferencia/lareferencia-platform.git
-# echo "Changing directory to lrharvester-platform"
-# cd lareferencia-platform || exit
-# sed "s/github.com/$user:$password@github.com/g" pull_and_build.sh > pull_and_build.sh.tmp && mv pull_and_build.sh.tmp pull_and_build.sh
-# echo "Installing lrharvester"
-# bash pull_and_build.sh ibict
-# echo "Finished installation"
+# ./build.sh
+# echo "Finished building lrharvester"
+
+echo "Removing lrharverster folder"
+rm -rf lrharvester/
+echo "Creating lrharvester folder"
+mkdir -p lrharvester
+echo "Changing diretory to lrharvester"
+cd lrharvester || exit
+echo "Downloading lrharvester"
+git clone https://$user:$password@github.com/lareferencia/lareferencia-platform.git
+echo "Changing directory to lrharvester-platform"
+cd lareferencia-platform || exit
+sed "s/github.com/$user:$password@github.com/g" pull_and_build.sh > pull_and_build.sh.tmp && mv pull_and_build.sh.tmp pull_and_build.sh
+echo "Installing lrharvester"
+bash pull_and_build.sh ibict
+echo "Finished installation"
+
 echo "Adjusting configurations"
 cd .. || exit
 sed -i "s/localhost:5432/postgres:5432/g" $model
